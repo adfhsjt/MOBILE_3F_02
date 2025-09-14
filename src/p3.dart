916b0f14b3feea299bcf2a80224faf2c11fe0634
adfhsjt <ahmadriski207@gmail.com>
2025-09-15 06:57:16 +0700

    sey

--- src/p3.dart
+++
@@ -1,9 +0,0 @@
-void main(List<String> args) {
-  for (int Index = 10; Index < 27; Index++) {
-    if (Index == 21)
-      break;
-    else if (Index > 1 || Index < 7)
-      continue;
-    print(Index);
-  }
-}
