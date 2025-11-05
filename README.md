b361c3467cfa78a1707d00e90a1f770342d0e783
adfhsjt <ahmadriski207@gmail.com>
2025-11-05 15:35:46 +0700

    W11: Soal 4

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Pemograman Mobile - Pertemuan 11   
 
 ## W11: Soal 1
+
 <img src="img/W11-Soal 1.png">
 
 ## W11: Soal 2
+
 <img src="img/W11-Soal 2.png">
 
 ## W11: Soal 3
+
 **Jawaban Pertanyaan:** 
 
 Pada bagian substring(0, 450), kode memotong isi respons string dan hanya mengambil 450 karakter
@@ -15,5 +18,20 @@ pertama agar teks yang ditampilkan tidak terlalu panjang, namun jika panjang tek
 Sementara catchError(...) merupakan penanganan kesalahan untuk operasi Future yang asinkron, blok ini akan dijalankan jika getData() gagal (misalnya jaringan/SSL/URL tidak valid) atau jika ada exception di dalam then, lalu UI diperbarui lewat setState dengan pesan “An error occurred”, perlu diingat status HTTP non‑200 tidak otomatis memicu catchError, jadi tetap cek statusCode di then.
 
 **Hasil Running**:
+
 <img src="img/W11-Soal 3.gif">
+
+## W11: Soal 4
+
+**Jawaban Pertanyaan:**
+
+Langkah 1: Tiga fungsi Dart asinkron (`Future<int>`) masing‑masing menunggu Tiga fungsi Dart asinkron (Future<int>) masing‑masing menunggu 3 detik menggunakan Future.delayed lalu mengembalikan nilai 1, 2, dan 3.
+
+Langkah 2: Fungsi async count menunggu (await) tiga fungsi asinkron, menjumlahkan hasilnya ke total, lalu memanggil setState untuk memperbarui UI dengan result = total.toString().
+
+**Hasil Running:**
+
+<img src="img/W11-Soal 4.gif">
+
+
 
---
+++ src/books/lib/main.dart
@@ -0,0 +1,92 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Future Demo Hannan',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String result = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Back from the Future Hannan')),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ElevatedButton(
+              child: const Text('GO!'),
+              onPressed: () {
+                count();
+              },
+            ),
+            const Spacer(),
+            Text(result),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Future<Response> getData() async {
+  //   const authority = 'www.googleapis.com';
+  //   const path = '/books/v1/volumes/Ka0eEAAAQBAJ';
+  //   Uri url = Uri.https(authority, path);
+  //   return http.get(url);
+  // }
+
+  Future<int> returnOneAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 1;
+  }
+
+  Future<int> returnTwoAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 2;
+  }
+
+  Future<int> returnThreeAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 3;
+  }
+
+  Future count() async {
+    int total = 0;
+    total = await returnOneAsync();
+    total += await returnTwoAsync();
+    total += await returnThreeAsync();
+    setState(() {
+      result = total.toString();
+    });
+  }
+}
